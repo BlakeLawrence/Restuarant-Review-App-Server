f0f4b12a18f14b3085a77f5e395b46279143ec62
BlakeLawrence <blake-lawrence@hotmail.co.uk>
2022-09-03 09:43:52 +0100

    added more logic to try solve cors issues

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
-app.all("/", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://restuarant-reviews.netlify.app/"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
